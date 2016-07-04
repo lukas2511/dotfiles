51322b44f3bcf3d69773f57bba1bab7a040ddf16
Lukas Schauer <lukas@schauer.so>
2016-07-05 00:37:37 +0200

    enable colors in minicom

--- zsh.d/colors.zsh
+++ zsh.d/colors.zsh
@@ -3,6 +3,9 @@ autoload colors; colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 
+# minicom
+export MINICOM="-m -c on"
+
 # colorized manpages
 man() {
 	env \
