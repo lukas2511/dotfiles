8726c781a3732ea72524347b1c77827dcb27bc16
Lukas Schauer <lukas@schauer.so>
2017-02-05 14:25:16 +0100

    blackmagic: disable gdb confirmation

--- zsh.d/aliases.zsh
+++ zsh.d/aliases.zsh
@@ -5,6 +5,7 @@
 alias ip4="ip -4"
 alias ip6="ip -6"
 
-alias blackmagic="arm-none-eabi-gdb -ex 'set target-async on' -ex 'set confirm on' -ex 'set mem inaccessible-by-default off' -ex 'target extended /dev/ttyBmpGdb' -ex 'monitor version' -ex 'monitor swdp_scan' -ex 'attach 1'"
-alias blackmagic-flash="blackmagic -ex 'load' -ex 'detach' -ex 'quit'"
+alias blackmagic="arm-none-eabi-gdb -ex 'set target-async on' -ex 'set confirm off' -ex 'set mem inaccessible-by-default off' -ex 'target extended /dev/ttyBmpGdb' -ex 'monitor version' -ex 'monitor swdp_scan' -ex 'attach 1'"
+alias blackmagic-flash="blackmagic --batch-silent -ex 'load' -ex 'detach' -ex 'quit'"
+
 
