081c7d69e5287c06846476fdb036ee5335b7d7b4
Lukas Schauer <lukas@schauer.so>
2017-04-17 02:53:56 +0200

    ifconfig replacement

--- zsh.d/aliases.zsh
+++ zsh.d/aliases.zsh
@@ -5,6 +5,14 @@
 alias ip4="ip -4"
 alias ip6="ip -6"
 
+ifconfig() {
+	(
+		((ip -br -c a && ip -br -c l) | sort) || \
+		ip a ||
+		ifconfig
+	) 2> /dev/null
+}
+
 alias blackmagic="arm-none-eabi-gdb -ex 'set target-async on' -ex 'set confirm off' -ex 'set mem inaccessible-by-default off' -ex 'target extended /dev/ttyBmpGdb' -ex 'monitor version' -ex 'monitor swdp_scan' -ex 'attach 1'"
 alias blackmagic-flash="blackmagic --batch-silent -ex 'load' -ex 'detach' -ex 'quit'"
 
