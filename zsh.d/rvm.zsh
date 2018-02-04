3085a81710be679f3e200fc948af7e7129301fe0
Lukas Schauer <lukas@schauer.so>
2018-02-04 23:41:40 +0100

    rvm breaks and slows down stuff, don't load by default

--- zsh.d/rvm.zsh
+++ zsh.d/rvm.zsh
@@ -1,5 +1,7 @@
-if [ -e ~/.rvm/scripts/rvm ]; then
-	. ~/.rvm/scripts/rvm
-elif [ -e /usr/share/rvm/scripts/rvm ]; then
-	. /usr/share/rvm/scripts/rvm
-fi
+load_rvm() {
+	if [ -e ~/.rvm/scripts/rvm ]; then
+		. ~/.rvm/scripts/rvm
+	elif [ -e /usr/share/rvm/scripts/rvm ]; then
+		. /usr/share/rvm/scripts/rvm
+	fi
+}
