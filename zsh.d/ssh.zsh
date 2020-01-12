32a0c4af6f9519a235eb89c711fe45cffff56af3
Lukas Schauer <lukas@schauer.so>
2020-01-12 18:21:38 +0100

    added ssh alias to interact with devices from the stone age

--- zsh.d/ssh.zsh
+++ zsh.d/ssh.zsh
@@ -1,2 +1,13 @@
+# for connecting to temporary systems (e.g. initial router configurations) while not cluttering known_hosts
 alias stupid_ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o VerifyHostKeyDNS=no"
 alias stupid_scp="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o VerifyHostKeyDNS=no"
+compdef stupid_ssh=ssh
+compdef stupid_scp=scp
+
+# interact with devices from the stone age
+alias old_ssh="ssh -o 'KexAlgorithms diffie-hellman-group1-sha1' -o 'HostKeyAlgorithms +ssh-dss' -o 'Ciphers aes128-cbc'"
+alias stupid_old_ssh="ssh -o 'KexAlgorithms diffie-hellman-group1-sha1' -o 'HostKeyAlgorithms +ssh-dss' -o 'Ciphers aes128-cbc'"
+alias stupid_old_scp="scp -o 'KexAlgorithms diffie-hellman-group1-sha1' -o 'HostKeyAlgorithms +ssh-dss' -o 'Ciphers aes128-cbc'"
+compdef old_ssh=ssh
+compdef stupid_old_ssh=ssh
+compdef stupid_old_scp=scp
