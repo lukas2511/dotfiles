eeaa41e253f2305f5ce6fc93f9151d6304a5e6e6
Lukas Schauer <lukas@schauer.so>
2020-01-12 18:51:19 +0100

    added ~/.dotfiles-local/bin to path

--- zsh.d/homebin.zsh
+++ zsh.d/homebin.zsh
@@ -1,6 +1,9 @@
 if [ -e "${HOME}/.dotfiles/bin" ]; then
 	export PATH="${HOME}/.dotfiles/bin:${PATH}"
 fi
+if [ -e "${HOME}/.dotfiles-local/bin" ]; then
+	export PATH="${HOME}/.dotfiles-local/bin:${PATH}"
+fi
 if [ -e "${HOME}/.bin" ]; then
 	export PATH="${HOME}/.bin:${PATH}"
 fi
